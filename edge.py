5b0eeb7b56d9b189cce8092bc45991a4707d0a74
YanaLustina <yana.lustina@gmail.com>
2018-03-02 12:58:50 -0600

    Add comments

--- edge.py
+++ edge.py
@@ -15,21 +15,27 @@ def roi(img, vertices):
 
     return masked
 
-def main():
 
+def main():
+    """
+
+
+    :return:
+    """
 
     while True:
-        printscreen = np.array(ImageGrab.grab(bbox=(0, 20, 800, 620)))
-        grayscale = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(grayscale, threshold1=200, threshold2=300)
+        printscreen = np.array(ImageGrab.grab(bbox=(0, 20, 800, 620)))  # captures screen
+        grayscale = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)  # screen is grayscale
+        edges = cv2.Canny(grayscale, threshold1=200, threshold2=300)  # creates Canny edges
 
         vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32)
-        r_of_int = roi(edges, [vertices])
+        r_of_int = roi(edges, [vertices])  # region of interest
 
         cv2.imshow('Edges', r_of_int)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+
 
 if __name__ == "__main__":
     main()
