98509dbfb2dfd9efbb7e7fdc7775880d425562f9
Yana Lustina <yana.lustina@gmail.com>
2018-03-14 11:35:27 -0500

    Update GTAV1.py

--- GTAV1.py
+++ GTAV1.py
@@ -4,6 +4,24 @@ import cv2
 import time
 
 
+# def draw_lines(img, lines):
+#     for line in lines:
+#         coords = line[0]
+#         cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+
+def process(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+
+    vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32)
+    processed_img = roi(processed_img, [vertices])
+
+    # lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 20, 15)
+    # draw_lines(processed_img, lines)
+
+    return processed_img
+
+
 def roi(img, vertices):
     mask = np.zeros_like(img)
     cv2.fillPoly(mask, vertices, 255)
@@ -11,19 +29,13 @@ def roi(img, vertices):
     return masked
 
 
+
 def main():
+    while(True):
+        screen = np.array(ImageGrab.grab(bbox=(0, 20, 800, 620)))
 
-
-    while True:
-        printscreen = np.array(ImageGrab.grab(bbox=(0, 20, 800, 620)))
-        grayscale = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(grayscale, threshold1=200, threshold2=300)
-
-        vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32)
-
-        r_of_int = roi(edges, [vertices])
-
-        cv2.imshow('Edges', r_of_int)
+        new_screen = process(screen)
+        cv2.imshow('window', new_screen)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
