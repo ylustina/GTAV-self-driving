9aa1a27acad734ce0e1a89b9bacd4c6f00e1db66
Yana Lustina <yana.lustina@gmail.com>
2018-03-25 16:11:27 -0500

    Add Hough Lines

--- GTAV1.py
+++ GTAV1.py
@@ -1,25 +1,15 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
-import time
 
 
-# def draw_lines(img, lines):
-#     for line in lines:
-#         coords = line[0]
-#         cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
-
-def process(original_image):
-    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
-
-    vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32)
-    processed_img = roi(processed_img, [vertices])
-
-    # lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 20, 15)
-    # draw_lines(processed_img, lines)
-
-    return processed_img
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 3)
+    except:
+        pass
 
 
 def roi(img, vertices):
@@ -29,15 +19,30 @@ def roi(img, vertices):
     return masked
 
 
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
+
+    vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32)
+    processed_img = roi(processed_img, [vertices])
+
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 20, 15)
+    draw_lines(processed_img, lines)
+
+    return processed_img
+
 
 def main():
-    while(True):
+    while True:
         screen = np.array(ImageGrab.grab(bbox=(0, 20, 800, 620)))
+        new_screen = process_img(screen)
 
-        new_screen = process(screen)
         cv2.imshow('window', new_screen)
+
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+
 
 main()
