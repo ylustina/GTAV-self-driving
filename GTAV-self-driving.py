5460180f1295689d7cbe111aa992c1bebfd5219a
Yana Lustina <yana.lustina@gmail.com>
2018-03-27 11:54:48 -0500

    Add 'img_ops' and 'original_img' to screens

--- GTAV-self-driving.py
+++ GTAV-self-driving.py
@@ -3,14 +3,12 @@ import numpy as np
 from PIL import ImageGrab
 
 
-
 def roi(img, vertices):
     mask = np.zeros_like(img)
     cv2.fillPoly(mask, vertices, 255)
     masked = cv2.bitwise_and(img, mask)
 
     return masked
-
 
 
 # purple
@@ -30,21 +28,21 @@ def draw_lines(img, lines):
         pass
 
 
-
-
-
-def img_ops(original_image):
+def img_ops(image):
     # TODO look up docstrings PEP 257 & annotations PEP 450something
+
 
     """
     Note:
 
-    :param original_image:
+    :param original_img:
     :return:
 
     """
 
-    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    original_img = image
+
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
     processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
 
@@ -54,15 +52,16 @@ def img_ops(original_image):
     lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 20, 15)
     draw_lines(processed_img, lines)
 
-    return processed_img
+    return processed_img, original_img
 
 
 def main():
     while True:
         screen = np.array(ImageGrab.grab(bbox=(0, 20, 800, 620)))
-        new_screen = img_ops(screen)
+        new_screen, original_img = img_ops(screen)
 
-        cv2.imshow('window', new_screen)
+        cv2.imshow('img_ops', new_screen)
+        cv2.imshow('original_img', cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB))
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
@@ -71,5 +70,6 @@ def main():
 
 # if you're not running this file directly, don't run this code
 # TODO look up side effects
+
 if __name__ == "__main__":
     main()
